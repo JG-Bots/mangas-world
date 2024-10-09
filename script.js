60818e7d67ada9e15c9b8fe2fca183d4374312b8
JG-Bots <105572107+JG-Bots@users.noreply.github.com>
2024-10-09 17:30:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const navbarLinks = document.getElementById('navbarLinks');
+  
+  // Adiciona ou remove a classe 'active' para controlar a visibilidade
+  navbarLinks.classList.toggle('active');
+}
