013a26ed970e9f3c63922fa364208300877f4db0
JG-Bots <105572107+JG-Bots@users.noreply.github.com>
2025-08-18 12:50:04 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-  const navbarLinks = document.getElementById('navbarLinks');
-  
-  // Adiciona ou remove a classe 'active' para controlar a visibilidade
-  navbarLinks.classList.toggle('active');
-}
